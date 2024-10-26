c0611559ef3f9c7664ac4f9ca79477c409422a9f
Rodrigo <101797599+R0drigo-0@users.noreply.github.com>
2024-10-26 17:13:51 +0200

    Extract snort and clamav

--- js/iocextractor.js
+++ js/iocextractor.js
@@ -1,8 +1,8 @@
 const iocPatterns = {
-  ipv4: /(?:\d{1,3}[.]\d{1,3}[.]\d{1,3}[.]\d{1,3}|\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})(?:\/\d{1,2})?/g,
+  ipv4: /(?:(?:25[0-5]|2[0-4][0-9]|1[0-9]{2}|[1-9]?[0-9])\.){3}(?:25[0-5]|2[0-4][0-9]|1[0-9]{2}|[1-9]?[0-9])(?:\/\d{1,2})?/g,
   ipv6: /\b([0-9a-fA-F]{1,4}:){7,7}[0-9a-fA-F]{1,4}(\/[0-9]{1,3})?\b/g,
   macAddress: /(?:[0-9A-Fa-f]{2}[:-]){5}(?:[0-9A-Fa-f]{2})/g,
-  email: /\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,7}\b/g,
+  email: /\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,7}\b/g,
   url: /https?:\/\/(?:www\.|[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z]{2,6})(?:[-a-zA-Z0-9@:%_\+.~#?&//=]*)/g,
   filePath:
     /(?:[a-zA-Z]:\\(?:[^<>:"/\\|?*\x00-\x1F]+\\)*[^<>:"/\\|?*\x00-\x1F]*)|(?:\/(?:[^<>:"/\\|?*\x00-\x1F]+\/)*[^<>:"/\\|?*\x00-\x1F]*)/g,
@@ -13,36 +13,62 @@ const iocPatterns = {
   md5: /\b[A-Fa-f0-9]{32}\b/g,
   cve: /\bCVE-\d{4}-\d{1,7}\b/g,
   mitre: /\bT\d{4}\b/g,
+  clamav: /\b[A-Za-z0-9]+\.[A-Za-z0-9]+\.[A-Za-z0-9]+-[0-9]+-[0-9]+\b/g,
 };
 
 const extractYaraRules = (text) => {
-  const yaraPattern = /rule\s+[A-Za-z0-9._]+\s*:\s*[A-Za-z0-9._]+\s*{/g; // Match the start of YARA rule
+  const yaraPattern = /rule\s+[A-Za-z0-9._]+\s*:\s*[A-Za-z0-9._]+\s*{/g;
   const matches = text.match(yaraPattern) || [];
   const validYaraRules = [];
 
   for (const match of matches) {
-    const startIndex = text.indexOf(match); // Start index of the match
-    let openBraces = 1; // Start with one open brace for the current match
-    let endIndex = startIndex + match.length; // Initialize end index
+    const startIndex = text.indexOf(match);
+    let openBraces = 1;
+    let endIndex = startIndex + match.length;
 
     while (openBraces > 0 && endIndex < text.length) {
       const char = text[endIndex];
 
-      if (char === "{") {
-        openBraces++; // Increment for each open brace
-      } else if (char === "}") {
-        openBraces--; // Decrement for each close brace
-      }
+      if (char === "{") openBraces++;
+      else if (char === "}") openBraces--;
 
       endIndex++;
     }
 
     if (openBraces === 0) {
-      validYaraRules.push(text.slice(startIndex, endIndex)); // Extract full rule
+      validYaraRules.push(text.slice(startIndex, endIndex));
     }
   }
 
   return validYaraRules;
+};
+
+const extractSnortRules = (text) => {
+  const snortPattern =
+    /alert\s+(tcp|udp|icmp|ip)\s+(any|[a-zA-Z0-9_.$]+)\s+(any|[a-zA-Z0-9_.$]+)\s*->\s+(any|[a-zA-Z0-9_.$]+)\s+(any|[a-zA-Z0-9_.$]+)\s*\(([^()]*;?)+\)/g;
+  const matches = text.match(snortPattern) || [];
+  const validSnortRules = [];
+
+  for (const match of matches) {
+    const startIndex = text.indexOf(match);
+    let openBraces = 1;
+    let endIndex = startIndex + match.length;
+
+    while (openBraces > 0 && endIndex < text.length) {
+      const char = text[endIndex];
+
+      if (char === "(") openBraces++;
+      else if (char === ")") openBraces--;
+
+      endIndex++;
+    }
+
+    if (openBraces === 0) {
+      validSnortRules.push(text.slice(startIndex, endIndex));
+    }
+  }
+
+  return validSnortRules;
 };
 
 const extractIOCs = (text) => {
@@ -52,8 +78,7 @@ const extractIOCs = (text) => {
   }
 
   results["yara"] = extractYaraRules(text);
-  results["clamav"] = [];
-  results["snort"] = [];
+  results["snort"] = extractSnortRules(text);
 
   return results;
 };
